f19379cfe10939486b138e9f52430a454f855d2c
Marina Roitman <55516348+MarinaRoitman@users.noreply.github.com>
2024-05-06 21:50:19 -0300

    Cambio users.json a .tsx

--- SHOPPEAR/src/__mocks__/users.tsx
+++ SHOPPEAR/src/__mocks__/users.tsx
@@ -1,29 +1,26 @@
-{
-    "usuarios":
-    [
-        {
+const users = [
+    {
         "id": 1,
         "nombre": "Carolina",
         "apellido": "Guevarin",
         "mail": "caritoG@gmail.com",
         "contrasena": "12345",
         "direccion": "Av Uade 234"
-        },
-        {
+    },
+    {
         "id": 2,
         "nombre": "Harland David",
         "apellido": "Sanders",
         "mail": "coronelsanders@gmail.com",
         "contrasena": "amoelpollo",
         "direccion": "Av pollo 111"
-        },
-        {
+    },
+    {
         "id": 3,
         "nombre": "Rafael",
         "apellido": "Nadal",
         "mail": "rafanadal@gmail.com",
         "contrasena": "tenis123",
         "direccion": "Av Corrientes 4491"
-        }
-    ]
-}
+    }
+]
